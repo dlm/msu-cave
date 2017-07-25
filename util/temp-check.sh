ca8106e0cd72a25364bc6558a3044c1767c04809
Brendan Kristiansen <brendankristiansen@icloud.com>
2017-07-24 19:04:02 -0600

    Update temp reader

--- util/temp-check.sh
+++ util/temp-check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Raspberry Pi Temperature Monitor."
+echo "Press CTRL + c to end."
 while :
 do
-	cat /sys/class/thermal/thermal_zone0/temp
+	raw_data=$(cat /sys/class/thermal/thermal_zone0/temp)
+	temp= $raw_Data/1000
+	echo "CPU Temp: $temp degrees Celsius"
 	sleep 3
 done
