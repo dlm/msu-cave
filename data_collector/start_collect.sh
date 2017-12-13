702f42ef96e22800b5b2422a59e345dc8010758e
David Millman <dave@cs.unc.edu>
2017-12-13 14:22:07 -0700

    Add extra error checking

--- data_collector/start_collect.sh
+++ data_collector/start_collect.sh
@@ -3,15 +3,20 @@
 # abort on error
 set -e
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Must specify exp directory'
-    echo "  $0 expDir"
-    exit 0
+if [[ $# -eq 0 ]]; then
+    echo "Must specify exp directory"
+    echo "  $0 directory_name"
+    exit 1
 fi
 
-
-
 expDir=$1
+
+if [[ -d $expDir ]]; then
+    echo "Directory already exists... terminating"
+    echo "  $0 expDir"
+    exit 2
+fi
+
 mkdir "$expDir"
 for i in `seq 1 5`; do
     ./data_collector.js --pod=$i > "$expDir/pod${RANDOM}.log" &
